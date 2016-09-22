799bbe532100b87b23067e3e55132ad97b27852d
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2016-09-22 09:44:34 -0700

    Advance driver and add 4 panel plot

--- src/python/devel/mean_climate_maps/scripts/mk_clim_dif_maps-driver.py
+++ src/python/devel/mean_climate_maps/scripts/mk_clim_dif_maps-driver.py
@@ -17,54 +17,68 @@ if m == 'oceanonly':
 
 if m == 'crunchy':
  basedir = '/export_backup/gleckler1/'
+ basedir = '/work/lee1043/cdat/pmp/' ## FOR TEST -jwlee
 
-plots_outdir = '/work/gleckler1/processed_data/clim_plots/'
+#plots_outdir = '/work/gleckler1/processed_data/clim_plots/'
+plots_outdir = '/work/lee1043/cdat/pmp/clim_plots/' ## FOR TEST -jwlee
 
 
 vars = ['pr','rlut','tas','rt']
 vars = ['pr']
 
+seasons = ['djf', 'mam', 'jja', 'son']
+
 # Load the obs dictionary
-fjson = open(
-        os.path.join(
-            sys.prefix,
-            "share",
-            "pmp",
-            "obs_info_dictionary.json"))
+#fjson = open(
+#        os.path.join(
+#            sys.prefix,
+#            "share",
+#            "pmp",
+#            "obs_info_dictionary.json"))
+fjson = open('/export_backup/lee1043/git/pcmdi_metrics/doc/obs_info_dictionary.json') ## FOR TEST -jwlee
 obs_dic = json.loads(fjson.read())
 fjson.close()
 
 #execfile('/export/gleckler1/git/pcmdi_metrics/src/python/pcmdi/seasonal_mean.py')
-
-
+execfile('./plot_map_4panel.py')
 
 # OBS path
 opathin = basedir + 'processed_data/obs/atm/mo/VAR/OBS/ac/VAR_OBS_000001-000012_ac.nc'
 
 # MOD path
-
 mpathin = '/work/gleckler1/processed_data/metrics_package/interpolated_model_clims_historical/global/cmip5.MOD.historical.r1i1p1.mo.Amon.VAR.ver-1.1980-2005.interpolated.linear.2.5x2.5.global.AC.nc'
 
 po = plots_outdir
 subs = ['',era,exp]
 for sub in subs: 
- po = po + '/' + sub
- try:
-  os.mkdir(po)
- except:
-  pass
-
-
-v = vcs.init()
+  po = po + '/' + sub
+  try:
+    os.mkdir(po)
+  except:
+    pass
 
 for var in vars:
 
+   #==============================================================================
+   # Observation
+   #------------------------------------------------------------------------------
    obsd = obs_dic[var]['default']
    obst = string.replace(opathin,'VAR',var)
    obst = string.replace(obst,'OBS',obsd)
    fo = cdms2.open(obst)
    do = fo(var)
 
+   ogrid = do.getGrid()
+
+   # Seasonal climatology of observation
+   obs = {}
+   for season in seasons:
+     obs[season] = pcmdi_metrics.pcmdi.seasonal_mean.compute(do,season)
+
+   #==============================================================================
+   # Models
+   #------------------------------------------------------------------------------
+   # Get list of models
    mods = []
    mpatht = string.replace(mpathin,'MOD','*')
    mpatht = string.replace(mpatht,'VAR',var)
@@ -73,46 +87,86 @@ for var in vars:
      mod = string.split(l,'.')[1] 
      if mod not in mods: mods.append(mod) 
 
+   # For test...
+   mods = mods[0:3]
+   #mods = mods[0:1]
+
+   # Dictionary to save
+   fld = {} # Model's climatology field
+   dif = {} # Difference btw. model and obs
+   mmm = {} # Multi model ensemble
+
+   for season in seasons:
+     mmm[season] = 0
+
+   #------------------------------------------------------------------------------
+   # Calculate seasonal mean and its difference against obs from MMM and individual models 
+   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    for mod in mods:
-        mpatht = string.replace(mpathin,'MOD',mod)
-        mpatht = string.replace(mpatht,'VAR',var)
 
-        fm = cdms2.open(mpatht)
-        dm = fm(var)
-## RGRID OBS FOR FIRST MOD
-        if mod == mods[0]:
-          mgrid = dm.getGrid()
-          dor = do.regrid(mgrid)
-          print 'done regrindding obs'
+     fld[mod] = {} # Model's climatology field
+     dif[mod] = {} # Difference btw. model and obs
 
-        print mod,' ', dm.shape, dor.shape
+     mpatht = string.replace(mpathin,'MOD',mod)
+     mpatht = string.replace(mpatht,'VAR',var)
 
-        mod_djf = pcmdi_metrics.pcmdi.seasonal_mean.compute(dm,'djf')
-        mod_mam = pcmdi_metrics.pcmdi.seasonal_mean.compute(dm,'mam')
-        mod_jja = pcmdi_metrics.pcmdi.seasonal_mean.compute(dm,'jja')
-        mod_son = pcmdi_metrics.pcmdi.seasonal_mean.compute(dm,'son')
+     fm = cdms2.open(mpatht)
+     dm = fm(var)
 
-        obs_djf = pcmdi_metrics.pcmdi.seasonal_mean.compute(dor,'djf')
-        obs_mam = pcmdi_metrics.pcmdi.seasonal_mean.compute(dor,'mam')
-        obs_jja = pcmdi_metrics.pcmdi.seasonal_mean.compute(dor,'jja')
-        obs_son = pcmdi_metrics.pcmdi.seasonal_mean.compute(dor,'son')
+     print mod
 
-        dif_djf = MV2.subtract(mod_djf,obs_djf)
-        dif_mam = MV2.subtract(mod_djf,obs_mam)
-        dif_jja = MV2.subtract(mod_djf,obs_jja)
-        dif_son = MV2.subtract(mod_djf,obs_son)
+     for season in seasons:
 
-        pout = plots_outdir + '/' + era + '/' + exp + '/' + var
-        try:
-          os.mkdir(pout) 
-        except:
-         pass
+       # Seasonal climatology
+       fld[mod][season] = pcmdi_metrics.pcmdi.seasonal_mean.compute(dm,season)
 
-        go = pout + '/' + var + '.' + mod 
-         
-        v.plot(dif_djf, bg=1)
-        v.png(go)
+       # Regrid to observational grid
+       fld_regrid = fld[mod][season].regrid(ogrid, regridTool='regrid2', mkCyclic=True)
 
-        time.sleep(.5)
-        v.clear()
-#       w = sys.stdin.readline()
+       # Accumulate for MMM
+       mmm[season] = MV2.add(mmm[season],fld_regrid)
+
+       # Get difference field
+       dif[mod][season] = MV2.subtract(fld_regrid,obs[season])
+       dif[mod][season].id = 'Diff_'+season.upper()+'_'+var
+
+   #------------------------------------------------------------------------------
+   # Get multi-model mean (MMM)
+   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   mmm_dif = {}
+   for season in seasons:
+     mmm[season] = mmm[season]/float(len(mods))
+
+     mmm_dif[season] = mmm[season] - obs[season] 
+     mmm_dif[season] = MV2.subtract(mmm[season],obs[season]) 
+     mmm_dif[season].id = 'Diff_'+season.upper()+'_'+var
+
+   #------------------------------------------------------------------------------
+   # Create 4 panel plots: model, obs, model-obs, mmm-obs
+   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   pout = plots_outdir + '/' + era + '/' + exp + '/' + var
+   try:
+     os.mkdir(pout) 
+   except:
+     pass
+
+   seasons = seasons[0:1] ## For test!!!
+
+   for season in seasons:
+     if var == 'pr' and do.units == 'kg m-2 s-1' and dm.units == 'kg m-2 s-1':
+       obs[season] = obs[season] * 86400.  
+       obs[season].units = 'mm d-1'
+       mmm[season] = mmm[season] * 86400.
+       mmm[season].units = 'mm d-1'
+       mmm_dif[season] = mmm_dif[season] * 86400.
+       mmm_dif[season].units = 'mm d-1'
+     for mod in mods:
+       go = pout + '/' + var +'.' + season + '.' + mod 
+       debug = True
+       if var == 'pr' and do.units == 'kg m-2 s-1' and dm.units == 'kg m-2 s-1':
+         fld[mod][season] = fld[mod][season] * 86400.  
+         fld[mod][season].units = 'mm d-1'
+         dif[mod][season] = dif[mod][season] * 86400.  
+         dif[mod][season].units = 'mm d-1'
+
+       plot_4panel(debug, var, season, mod, fld[mod][season], obs[season], dif[mod][season], mmm_dif[season], go)
---
+++ src/python/devel/mean_climate_maps/scripts/plot_map_4panel.py
@@ -0,0 +1,199 @@
+#===========================================================================================
+def plot_4panel(debug, mode, season, model, s1, s2, s3, s4, output_file_name):
+#-------------------------------------------------------------------------------------------
+  import EzTemplate, vcs  
+  import string
+
+  if debug is None:
+    debug = True
+
+  ## Input fields ---
+  s = []
+  s.append(s1)
+  s.append(s2)
+  s.append(s3)
+  s.append(s4)
+
+  ## Initialize VCS ---
+  if debug:
+    canvas = vcs.init(geometry=(1000,800)) # Show canvas
+  if not debug:
+    canvas = vcs.init(geometry=(1000,800),bg=1) # Plotting in background mode
+
+  ## Canvas setup ---
+  canvas.open()
+  canvas.drawlogooff()
+  #canvas.setcolormap('bl_to_darkred')
+
+  ## iso setup ---
+  iso = canvas.createisofill()
+  iso, xtra = setup_iso(mode, iso, diff=False)
+
+  iso_diff = canvas.createisofill()
+  iso_diff, xtra = setup_iso(mode, iso_diff, diff=True)
+
+  ## Increase label text size ---
+  my_template = vcs.createtemplate()
+
+  tick_text = vcs.createtext()
+  tick_text.height = 22
+  tick_text.valign = "half"
+  tick_text.halign = "center" 
+  my_template.xlabel1.textorientation = tick_text.To_name
+  my_template.xlabel1.texttable = tick_text.Tt_name
+
+  tick_text2 = vcs.createtext()
+  tick_text2.height = 22
+  tick_text2.valign = "half"
+  tick_text2.halign = "right" 
+  my_template.ylabel1.textorientation = tick_text2.To_name
+  my_template.ylabel1.texttable = tick_text2.Tt_name
+
+  tick_text3 = vcs.createtext()
+  tick_text3.height = 25
+  tick_text3.valign = "half"
+  tick_text3.halign = "center" 
+  my_template.legend.textorientation = tick_text3.To_name
+  my_template.legend.texttable = tick_text3.Tt_name
+
+  ## EzTemplate ---
+  M = EzTemplate.Multi(template=my_template, rows=2,columns=2)  
+
+  # Legend colorbar ---
+  #M.legend.stretch = 1.5 # 150% of width (for middle one) --- in case use local legend
+  #M.legend.stretch = 0.8 # 150% of width (for middle one) --- in case use global legend
+  M.legend.thickness = 0.4 # Thickness of legend color bar
+  M.legend.direction = 'horizontal' 
+  #M.legend.direction = 'vertical' 
+
+  # Border margin for entire canvas ---
+  M.margins.top = .15
+  M.margins.bottom = .10
+  M.margins.left = .05  
+  M.margins.right = .05  
+
+  # Interval spacing between subplots ---
+  M.spacing.horizontal = .05
+  M.spacing.vertical = .10
+
+  # Title on top ---
+  plot_title = vcs.createtext()
+  plot_title.x = .5
+  plot_title.y = .97
+  plot_title.height = 30
+  plot_title.halign = 'center'
+  plot_title.valign = 'top'
+  plot_title.color = 'black'
+  plot_title.string = mode + ': ' + season
+  canvas.plot(plot_title)
+
+  # Plot subplots ---
+  left_pos = 0
+  for i in range(4):  
+    #t = M.get() # Use global colorbar
+    t = M.get(legend='local') # Use local colorbar
+    #if i%2 !=1:  
+    #  t.legend.priority=0 # Turn off legend  
+    #  left_pos = t.data.x1
+    #else:
+    ##  # Set legend (colorbar) position
+    #  t.legend.x1 = left_pos
+    #  t.legend.x2 = t.data.x2
+    #  t.legend.y1 = t.legend.y1 - 0.03
+    #  t.legend.y2 = t.legend.y2 - 0.03
+    t.legend.y1 = t.legend.y1 - 0.03
+    t.legend.y2 = t.legend.y2 - 0.03
+
+    t = setup_template(t)
+
+    #canvas.plot(s[i],t,iso)  
+    if i < 2: 
+      #canvas.plot(s[i](**xtra), t, iso)
+      canvas.plot(s[i], t, iso)
+    else:
+      #canvas.plot(s[i](**xtra), t, iso_diff)
+      canvas.plot(s[i], t, iso_diff)
+
+    # Titles of subplots ---
+    plot_title.height = 20
+    if i == 0: 
+      plot_title.string = model # multi-realization mean...
+      plot_title.x = .27
+      plot_title.y = .91
+    elif i == 1: 
+      plot_title.string = 'OBS'
+      plot_title.x = .73
+      plot_title.y = .91
+    elif i == 2: 
+      plot_title.string = 'Diff: '+model+' - OBS'
+      plot_title.x = .27
+      plot_title.y = .48
+    elif i == 3: 
+      plot_title.string = 'Diff: MMM - OBS'
+      plot_title.x = .73
+      plot_title.y = .48
+    canvas.plot(plot_title)
+   
+  #-------------------------------------------------
+  # Drop output as image file (--- vector image?)
+  #- - - - - - - - - - - - - - - - - - - - - - - - - 
+  canvas.png(output_file_name+'.png')
+
+  if not debug:
+    canvas.close()
+
+#===========================================================================================
+def setup_template(t):
+#-------------------------------------------------------------------------------------------
+  # Turn off no-needed information -- prevent overlap
+  t.blank(['title','mean','min','max','dataname','crdate','crtime',
+           'units','zvalue','tvalue','xunits','yunits','xname','yname'])
+  return(t)
+
+#===========================================================================================
+def setup_iso(mode, iso, diff):
+#-------------------------------------------------------------------------------------------
+  import string
+  import vcs
+
+  ## Setup color ---
+  if not diff: 
+    iso.colormap = 'bl_to_darkred'
+  else:
+    iso.colormap = 'bl_to_drkorang'
+    #iso.colormap = 'grn_to_magenta'
+    #iso.colormap = 'bl_to_darkred'
+
+  #if string.split(mode,'_')[0] == 'PDO':
+  #  iso.levels = [-0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5]
+  #else:
+  #  iso.levels = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+  iso.ext_1 = 'y' # control colorbar edge (arrow extention on/off)
+  iso.ext_2 = 'y' # control colorbar edge (arrow extention on/off)
+  #cols = vcs.getcolors(iso.levels)
+  #cols[6] = 139 # Adjsut to light red
+  #iso.fillareacolors = cols
+  #iso.missing = 0
+  iso.missing = 'white'
+
+  ## Map projection setup ---
+  #p = vcs.createprojection()
+  #if mode == 'NAM' or mode == 'SAM':
+  #  p.type = int('-3')
+  #elif mode == 'NAO' or mode == 'PNA' or mode == 'PDO' or mode == 'AMO':
+  #  p.type = 'lambert'
+  #elif mode == 'PDO_teleconnection' or mode == 'PDO_pseudo_teleconnection':
+  #  p.type = 'robinson'
+  #else:
+  #  p.type = int('-3')
+  #iso.projection = p
+
+  xtra = {}
+  #if mode == 'PDO_teleconnection' or mode == 'PDO_pseudo_teleconnection':
+  #  xtra = {}
+  #elif mode == 'SAM' or mode == 'SAM_teleconnection' or mode == 'SAM_pseudo_teleconnection':
+  #  xtra['latitude'] = (-90.0,0.0) # For Southern Hemisphere
+  #else:
+  #  xtra['latitude'] = (90.0,0.0) # For Northern Hemisphere
+
+  return(iso, xtra)
